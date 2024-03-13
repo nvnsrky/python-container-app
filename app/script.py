d6b6eb5e175eb4da9a8222d750c98ec6b3ee65e3
nvnsrky <nvnnur@gmail.com>
2024-03-13 11:18:34 +0300

    Açıklama: script.py dosyasında değişiklik yapıldı.

--- app/script.py
+++ app/script.py
@@ -1,2 +1,16 @@
 with open("output.txt", "w") as file:
     file.write("nevin beni konteynırdan üretti\n")
+
+import os
+
+# Eğer output.txt dosyası varsa, onu oku
+if os.path.exists("output.txt"):
+    with open("output.txt", "r") as file:
+        file_content = file.read()
+        print("output.txt dosyası bulundu ve içeriği okundu:")
+        print(file_content)
+else:
+    # Eğer output.txt dosyası yoksa, yeni bir input.txt dosyası oluştur ve içine "Aranan dosya bulunamadı" yaz
+    with open("input.txt", "w") as file:
+        file.write("Aranan dosya bulunamadı\n")
+    print("output.txt dosyası bulunamadı, yeni bir input.txt dosyası oluşturuldu ve içine 'Aranan dosya bulunamadı' yazıldı.")
